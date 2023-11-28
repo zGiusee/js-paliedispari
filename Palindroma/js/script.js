a57ef8b11a18f1b96b5168e2c80a37888e8f9bce
zGiusee <giusebelforte@gmail.com>
2023-11-28 15:27:59 +0100

    [UPDATE]STOPPING ES.1 e staring es 2

---
+++ Palindroma/js/script.js
@@ -0,0 +1,40 @@
+// FUNZIONI
+function reverse(word){
+   
+    let reverse = word[i].split('').reverse().join('').toLowerCase();
+    console.log(reverse)
+    
+}
+
+function paliGuesser(word){
+
+    for(let i=0; i<word.length; i++){
+        console.log(word[i])
+    }    
+
+    let normal = word[i].toLowerCase();
+    console.log(normal)
+}
+
+// PALINDROMA
+
+// Chiedo all'utente di inserire la parola
+
+let word = prompt("Inserisci la parola che credi possa essere palindroma.")
+
+let normal = paliGuesser(word);
+let reverseWord = reverse(word);
+
+if(normal === reverseWord){
+    console.log("PALINDROMA")
+}
+else{
+    console.log("NON PALIDNROMA")
+}
+
+
+
+
+
+
+
